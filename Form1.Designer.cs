350ea350c56f20c6b0bafe08acc258c6fea20ad8
Nick Carpenetti <nickcarpenetti@gmail.com>
2017-05-05 20:45:45 -0700

    initial commit. Started with a program that opens a connection and fetches simple_table

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace CapHo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.tbDBName = new System.Windows.Forms.TextBox();
+            this.QueryResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.QueryResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "URL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(400, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Port";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(73, 6);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(100, 20);
+            this.tbUser.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(193, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "DBName";
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(73, 32);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(100, 20);
+            this.tbPass.TabIndex = 13;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(196, 30);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 14;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // tbHost
+            // 
+            this.tbHost.Location = new System.Drawing.Point(73, 65);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(321, 20);
+            this.tbHost.TabIndex = 15;
+            this.tbHost.Text = "css475sp17-capho.c2b3wqyc8e3g.us-west-2.rds.amazonaws.com";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(432, 65);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(32, 20);
+            this.tbPort.TabIndex = 16;
+            this.tbPort.Text = "5432";
+            // 
+            // tbDBName
+            // 
+            this.tbDBName.Location = new System.Drawing.Point(249, 6);
+            this.tbDBName.Name = "tbDBName";
+            this.tbDBName.Size = new System.Drawing.Size(100, 20);
+            this.tbDBName.TabIndex = 17;
+            this.tbDBName.Text = "CapHo";
+            // 
+            // QueryResults
+            // 
+            this.QueryResults.AllowUserToAddRows = false;
+            this.QueryResults.AllowUserToDeleteRows = false;
+            this.QueryResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.QueryResults.Location = new System.Drawing.Point(12, 98);
+            this.QueryResults.Name = "QueryResults";
+            this.QueryResults.ReadOnly = true;
+            this.QueryResults.Size = new System.Drawing.Size(485, 254);
+            this.QueryResults.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 361);
+            this.Controls.Add(this.QueryResults);
+            this.Controls.Add(this.tbDBName);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Capitalism, Ho!";
+            ((System.ComponentModel.ISupportInitialize)(this.QueryResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.TextBox tbDBName;
+        private System.Windows.Forms.DataGridView QueryResults;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Npgsql;
+
+namespace CapHo
+{
+    public partial class Form1 : Form
+    {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server={0};Port={1};" +
+                    "User Id={2};Password={3};Database={4};",
+                    tbHost.Text, tbPort.Text, tbUser.Text,
+                    tbPass.Text, tbDBName.Text);
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT * FROM simple_table";
+                // data adapter making request from our connection
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                ds.Reset();
+                // filling DataSet with result from NpgsqlDataAdapter
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // connect grid to DataTable
+                QueryResults.DataSource = dt;
+                // since we only showing the result we don't need connection anymore
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+        }
+
+
+    }
+}
